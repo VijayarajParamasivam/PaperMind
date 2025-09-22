80fedec7078c4cb9529da35a378ae72c309a94ed
Vijayaraj Paramasivam <133317616+VijayarajParamasivam@users.noreply.github.com>
2025-09-22 22:43:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def main():
 
 
     st.caption("AI-powered PDF assistant")
-    st.info(f"Doubts cleared so far : **{500+}**")
+    st.info(f"Doubts cleared so far : **500+**")
 
     inject_chat_css()
 
